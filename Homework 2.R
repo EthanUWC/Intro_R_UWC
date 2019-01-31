4ed833e6a4d00a7790de81574e8ed3755d445792
EthanUWC <3540409@myuwc.ac.za>
2019-01-31 14:43:32 +0200

    Commit_homework_ext

--- Homework 2.R
+++ Homework 2.R
@@ -2,3 +2,13 @@
 
 lam <- read_csv("~/R/R/data/laminaria.csv")
 save(lam, file = "data/laminaria.RData")
+
+
+??ggsn
+
+#  Describe ggsn package
+#  North symbols and scale bars for maps created with 'ggplot' or 'ggmap'. Adds north symbols (18 options) and scale bars in kilometers to maps in geographic or metric coordinates created with 'ggplot' or 'ggmap'.
+
+??scale
+# Describe scale package
+# The scale package is a function within ggsn that offers generic plot scaling methods
