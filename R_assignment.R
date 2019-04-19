3bd70216f63e907e70caaa5a7554a78b299022f6
EthanUWC <47135465+EthanUWC@users.noreply.github.com>
2019-04-19 22:25:19 +0200

    R_assignment_commit

---
+++ R_assignment.R
@@ -0,0 +1,166 @@
+#Ethan Britz
+#3540409
+#R_assignment
+
+
+#load libraries
+library(fitdistrplus)
+library(tidyverse)
+library(plotly)
+library(logspline)
+library(dplyr)
+library(lubridate)
+
+
+#6.7.1
+
+head(r_dat)
+tail(r_dat)
+glimpse(r_dat)
+summary(r_dat)
+
+# Hypothesis: Male(A) foot size is larger than that of females(B)
+set.seed(666)
+r_dat <- data.frame(dat = c(rnorm(n = 100, mean = 12, sd = 3),
+                            rnorm(n = 100, mean = 8, sd = 2)),
+                    sample = c(rep("A", 100), rep("B", 100)))
+# Assigned the name r_dat to data frame I created containing normally distributed data. This was achieved using the rnorm function in which I could specify the total amount of samples(n), the mean and sd of the data.  
+shapiro.test(r_dat$dat) # P = 0.007, thus the data is normally distributed.
+
+t.test(r_dat$dat, mu = 30) #Performed a t-test on the dat column (male and female foot size). P= 2.2e-16, thus there is a significant difference in the data. Therefore, male foot size is larger than that of females.
+
+#7.4.1
+
+feed_1 <- c(60.8, 57.0, 65.0, 58.6, 61.7)
+feed_2 <- c(68.7, 67.7, 74.0, 66.3, 69.8)
+feed_3 <- c(102.6, 102.1, 100.2, 96.5)
+feed_4 <- c(87.9, 84.2, 83.1, 85.7, 90.3)
+
+bacon <- as.tibble(data.frame(
+  feed = c(
+    rep("Feed 1", length(feed_1)),
+    rep("Feed 2", length(feed_2)),
+    rep("Feed 3", length(feed_3)),
+    rep("Feed 4", length(feed_4))
+  ),
+  mass = c(feed_1, feed_2, feed_3, feed_4)
+))
+
+head(bacon)
+tail(bacon)
+glimpse(bacon)
+summary(bacon)
+
+# ANOVA 
+
+# QUESTION: Does feed type have an effect on the mass of pigs at the end of the experiment?
+# H0: There is NO difference in pig mass at the end of the experiments after being fed one of four diets
+# H1: There IS a difference in pig mass at the end of the experiments after being fed one of four diets
+
+bacon.aov1 <- aov(mass ~ feed, data = bacon)
+summary(pigs.aov)
+# Pr < 0.05, so we reject the null hypothesis
+# so, there is a difference in pig mass at the end of the experiment after being fed one of four diets
+
+# There is a difference, but which feeds have the effect?
+
+# Visualise with boxplots 
+
+bacon_box <- ggplot(data = bacon, aes(x = feed, y = mass, fill = feed )) +
+  geom_boxplot(notch = TRUE)
+# none of the feeds notched overlap... but this graph is just a suggestion
+
+# Tukey 
+
+TukeyHSD(bacon.aov1)
+# p-adj < 0.05 for all feeds: show all diets are different and significant
+# lwr and upr does not go across zero in all cases
+
+# Visualise this 
+
+plot(TukeyHSD(bacon.aov1))
+
+
+#7.4.2
+teeth <- datasets::ToothGrowth
+
+head(teeth)
+tail(teeth)
+glimpse(teeth)
+summary(teeth)
+
+# QUESTION: Does difference in doses of Vitamin C have an effect on the length of tooth growth in guinea pigs
+# H0: There is NO difference in tooth lengths of guinea pigs receiving one of three doses of vitamin C
+# H1: There IS a difference in tooth lengths of guinea pigs receiving one of three doses of vitamin C
+
+# Filter out only Vitamin C doses 
+
+teeth_vc <- ToothGrowth %>% 
+  filter(supp == "VC")
+
+# ANOVA 
+
+teeth.aov1 <- aov(len ~ as.factor(dose), data = teeth_vc)
+summary(teeth.aov1)
+# pr < 0.05, so we reject the null hypothesis
+# so, there is a difference in tooth lengths of guinea pigs receiving one of three doses of vitamin C
+
+# So which dosages have the effect?
+
+# Visualise this using a boxplot 
+
+ggplot(data = teeth_vc, aes(x = as.factor(dose), y = len, fill = as.factor(dose))) +
+  geom_boxplot(notch = TRUE)
+# none of the dose notches overlap... suggesting they are different
+
+# Tukey 
+
+TukeyHSD(teeth.aov1)
+# p-adj < 0.05, very low for all. so all doses are significant and different 
+# lwr and upr does not cross zero
+
+# Visualise this 
+
+plot(TukeyHSD(teeth.aov1))
+
+# 7.4.3 Exercise 3 
+
+teeth <- datasets::ToothGrowth
+
+# H0: interactions between supplement and dose have NO effect on length of teeth
+# H1: interactions between supplement and dose DO have an effect on length of teeth
+
+# looking at only length by supplement
+summary(aov(len ~ supp, data = teeth))
+
+TukeyHSD((aov(len ~ supp, data = teeth)))
+
+plot(TukeyHSD((aov(len ~ supp, data = teeth))))
+
+# dose was done in previous example, but running too many ANOVAS increases error
+# therefore
+
+# now to look at interactions BETWEEN factors
+summary(aov(len ~ supp * as.factor(dose), data = teeth)) 
+# pr < 0.05, so we reject the null hypothesis
+# so, interactions between supplement and dose DOES havae an effcet on length of teeth
+
+# So which have the effect?
+TukeyHSD((aov(len ~ supp * as.factor(dose), data = teeth)))
+
+plot(TukeyHSD((aov(len ~ supp * as.factor(dose), data = teeth))))
+# On plot, all combinations not crossing zero show which combinations of supplement and diet have the most effect on length of teeth  
+
+# Exercise 9.6 
+
+library(reshape2)
+library(ggplot2)
+library(RColorBrewer)
+
+nba_heat <- ggplot(nba, aes(x = Name, y = G, fill = G)) + #assigned the name nba_heat to a heat map generated by the ggplot command and assigned the player names on the x-axis and the player points on  the y-axis
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+  geom_tile() + #geom_tile plots values as tiles on a graph forming an aggregation resulting in a heat  map
+  scale_colour_gradientn(colours = c("#A5A94D", "#6FB16F", "#45B19B",
+                                     "#59A9BE", "#9699C4", "#CA86AD")) +#assigned a scale to the heat map
+  labs(x = "Name", y = "points per game") + # labelled the x-axis 'Name' and the y-axis 'points per game'
+  ggtitle("NBA player points per game heat map") #Assigned a title to the heat map
